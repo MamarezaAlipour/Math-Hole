cf46ffeb32ba185653b53006b7370985393cc532
Parisa Khaleghi <me@parikhaleghi.ir>
2022-07-01 01:35:29 +0430

    Update <stat>

--- include/mathhole/detail/stat_impl.hxx
+++ include/mathhole/detail/stat_impl.hxx
@@ -28,21 +28,46 @@ namespace mathhole {
 		inline Real correlation(std::int32_t n, Real const x[], Real const y[]);
 
         //----------------------------------------------------------------------------//
-		// Definitions (Soon)
+		// Definitions
 
-        //
+		//
 		// Arithmetic Mean
 
-        //
+		template <class Real>
+		inline Real arithmetic_mean(std::int32_t n, Real const x[]) {
+			return detail::arithmetic_mean_serial_impl(n, x);
+		}
+
+		//
 		// Variance
 
-        //
+		template <class Real>
+		inline Real variance(std::int32_t n, Real const x[]) {
+			return detail::variance_serial_impl(n, x);
+		}
+
+		//
 		// Standard Deviation
 
-        //
+		template <class Real>
+		inline Real standard_deviation(std::int32_t n, Real const x[]) {
+			return detail::standard_deviation_serial_impl(n, x);
+		}
+
+		//
 		// Covariance
 
-        //
+		template <class Real>
+		inline Real covariance(std::int32_t n, Real const x[], Real const y[]) {
+			return detail::covariance_serial_impl(n, x, y);
+		}
+
+		//
 		// Correlation
+
+		template <class Real>
+		inline Real correlation(std::int32_t n, Real const x[], Real const y[]) {
+			return detail::correlation_serial_impl(n, x, y);
+		}
     }
 }
