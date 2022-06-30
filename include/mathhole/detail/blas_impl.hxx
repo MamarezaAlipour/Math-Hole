49422fe8a068fde83d0aa55d07c90f81ac373f89
Parisa Khaleghi <me@parikhaleghi.ir>
2022-06-30 20:10:48 +0430

    const edit

--- include/mathhole/detail/blas_impl.hxx
+++ include/mathhole/detail/blas_impl.hxx
@@ -16,53 +16,53 @@ namespace mathhole {
 			//
 			// xAXPY (e.g. Linear Algebra Add)
 
-			extern "C" void mathhole_saxpy_(const std::int32_t* n, const float* alpha,
-											const float x[], const std::int32_t* incx, float y[],
-											const std::int32_t* incy);
+			extern "C" void mathhole_saxpy_(std::int32_t const* n, float const* alpha,
+											float const x[], std::int32_t const* incx, float y[],
+											std::int32_t const* incy);
 
-			extern "C" void mathhole_daxpy_(const std::int32_t* n, const double* alpha,
-											const double x[], const std::int32_t* incx, double y[],
-											const std::int32_t* incy);
+			extern "C" void mathhole_daxpy_(std::int32_t const* n, double const* alpha,
+											double const x[], std::int32_t const* incx, double y[],
+											std::int32_t const* incy);
 
 			//
 			// xNRM2 (e.g. Vector Euclidean Norm)
 
-			extern "C" float mathhole_snrm2_(const std::int32_t* n, const float x[],
-											 const std::int32_t* incx);
+			extern "C" float mathhole_snrm2_(std::int32_t const* n, float const x[],
+											 std::int32_t const* incx);
 
-			extern "C" double mathhole_dnrm2_(const std::int32_t* n, const double x[],
-											  const std::int32_t* incx);
+			extern "C" double mathhole_dnrm2_(std::int32_t const* n, double const x[],
+											  std::int32_t const* incx);
 
 			//
 			// xGEMV (e.g. General Matrix Vector Produt)
 
-			extern "C" void mathhole_sgemv_(const char* trans, const std::int32_t* m,
-											const std::int32_t* n, const float* alpha,
-											const float a[], const std::int32_t* lda,
-											const float x[], const std::int32_t* incx,
-											const float* beta, float y[], const std::int32_t* incy,
+			extern "C" void mathhole_sgemv_(char const* trans, std::int32_t const* m,
+											std::int32_t const* n, float const* alpha,
+											float const a[], std::int32_t const* lda,
+											float const x[], std::int32_t const* incx,
+											float const* beta, float y[], std::int32_t const* incy,
 											std::int32_t length_trans);
 
-			extern "C" void mathhole_dgemv_(const char* trans, const std::int32_t* m,
-											const std::int32_t* n, const double* alpha,
-											const double a[], const std::int32_t* lda,
-											const double x[], const std::int32_t* incx,
-											const double* beta, double y[],
-											const std::int32_t* incy, std::int32_t length_trans);
+			extern "C" void mathhole_dgemv_(char const* trans, std::int32_t const* m,
+											std::int32_t const* n, double const* alpha,
+											double const a[], std::int32_t const* lda,
+											double const x[], std::int32_t const* incx,
+											double const* beta, double y[],
+											std::int32_t const* incy, std::int32_t length_trans);
 
 			//
 			// xTRSV (e.g. Triangular Matrix Vector Solve)
 
-			extern "C" void mathhole_strsv_(const char* uplo, const char* trans, const char* diag,
-											const std::int32_t* n, const float a[],
-											const std::int32_t* lda, float x[],
-											const std::int32_t* incx, std::int32_t length_uplo,
+			extern "C" void mathhole_strsv_(char const* uplo, char const* trans, char const* diag,
+											std::int32_t const* n, const float a[],
+											std::int32_t const* lda, float x[],
+											std::int32_t const* incx, std::int32_t length_uplo,
 											std::int32_t length_trans, std::int32_t length_diag);
 
-			extern "C" void mathhole_dtrsv_(const char* uplo, const char* trans, const char* diag,
-											const std::int32_t* n, const double a[],
-											const std::int32_t* lda, double x[],
-											const std::int32_t* incx, std::int32_t length_uplo,
+			extern "C" void mathhole_dtrsv_(char const* uplo, char const* trans, char const* diag,
+											std::int32_t const* n, double const a[],
+											std::int32_t const* lda, double x[],
+											std::int32_t const* incx, std::int32_t length_uplo,
 											std::int32_t length_trans, std::int32_t length_diag);
 
 			//
