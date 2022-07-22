2e9cd46b00c6affb4cddc404ee3eba045baefa39
Parisa Khaleghi <me@parikhaleghi.ir>
2022-07-22 22:11:07 +0430

    Implement interface of Simd

---
+++ include/mathhole/cxx17/simd.hxx
@@ -0,0 +1,11 @@
+// Copyright (c) 2022 Parisa Khaleghi
+// All rights reserved
+
+#ifndef MATHHOLE_CXX17_SIMD_HXX
+#define MATHHOLE_CXX17_SIMD_HXX
+
+#include <mathhole/detail/simd_avx2.hxx>
+#include <mathhole/detail/simd_avx512.hxx>
+
+
+#endif
